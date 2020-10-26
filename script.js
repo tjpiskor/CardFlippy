be981d71e3f7c85e0fdc265921bb4c61821963a2
tjpiskor <70235547+tjpiskor@users.noreply.github.com>
2020-10-26 15:57:15 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,40 @@
+new Vue({
+  el: "#flashcards-app",
+  data: {
+    questionInput: "",
+    answerInput: "",
+    categoryInput: "",
+    cards:[
+      {
+        question:"What year was IUPUI founded?",
+        answer:"1969",
+        category:"history",
+        flipped:false
+      },
+      {
+        question:"What is 2+2?",
+        answer:"Fish",
+        category:"math",
+        flipped:false
+      },{
+        question:"Is mayonnaise an instrument?",
+        answer:"No",
+        category:"science",
+        flipped:false
+      }
+    ]
+  },
+  methods:{
+    addCard: function(){
+      this.cards.push({
+        question: this.questionInput,
+        answer: this.answerInput,
+        category: this.categoryInput,
+        flipped: false,
+      })
+        this.questionInput = "",
+        this.answerInput = "",
+        this.categoryInput = ""
+    }
+  }
+});
